0a44d18df159bf3f20911b9a4d08b0c6b8a9dd6b
ranjeet ghodke <ranjeetghodke10@gmail.com>
2023-03-29 10:49:34 +0530

    validation

--- backend/admin/login_validation.js
+++ backend/admin/login_validation.js
@@ -36,19 +36,17 @@ function admin_login_validation() {
 
 function new_brwhms_validation() {
     return [
-        check('org_id').not().isEmpty().withMessage('org_id, is required'),
-        check('location_id').not().isEmpty().withMessage('org_location_id is required'),
-        check('sys_id').not().isEmpty().withMessage('sys_id is required'),
-        check('device_name').not().isEmpty().withMessage('device_name is required'),
-        check('type_of_device').not().isEmpty().withMessage('type_of_device is required'),
+        check('name').not().isEmpty().withMessage('name, is required'),
         check('user_id').not().isEmpty().withMessage('user_id is required'),
+        check('type').not().isEmpty().withMessage('type is required'),
         check('lat').not().isEmpty().withMessage('lat is required'),
         check('lng').not().isEmpty().withMessage('Lng is required'),
-        check('a').not().isEmpty().withMessage('a is required'),
-        check('b').not().isEmpty().withMessage('b is required'),
+        check('sensor_gap').not().isEmpty().withMessage('sensor_gap is required'),
+        check('ac').not().isEmpty().withMessage('ac is required'),
         check('hmin').not().isEmpty().withMessage('hmin is required'),
-        check('hmax').not().isEmpty().withMessage('hmax is required')
-    ]
+        check('hmax').not().isEmpty().withMessage('hmax is required'),
+        check('location').not().isEmpty().withMessage('location is required')
+    ] 
     next();
 }
 
